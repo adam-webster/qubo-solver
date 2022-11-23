9f66f743f6f14788ebccffe1f5338b60a5cc6c98
Adam <adwebster@hotmail.co.uk>
2022-11-23 16:09:54 +0000

    should run with data

--- qubo_solver.py
+++ qubo_solver.py
@@ -1,17 +1,24 @@
 from functools import partial # for auto tuning chain strength
 from dwave.embedding.chain_strength import uniform_torque_compensation # for auto tuning chain strength
 from dwave.system import sample_qubo
+from dwave.system import EmbeddingComposite
 import pandas as pd
 
-device = BraketDWaveSampler(device_arn='arn:aws:braket:us-west-2::device/qpu/d-wave/Advantage_system6')
+from dwave.system import DWaveSampler
+qubo = pd.read_csv('credit_data_qubo.csv', header=None).to_dict()
+# Use a D-Wave system as the sampler
+sampler = DWaveSampler() 
+
+print("QPU {} was selected.".format(sampler.solver.name))
+
 #sampler = neal.SimulatedAnnealingSampler() # if we want to use simulator
-sampler = EmbeddingComposite(device)
+embedding_sampler = EmbeddingComposite(sampler)
 
 
 chain_strength = partial(uniform_torque_compensation, prefactor=2) # this auto-tunes the chain strength
 #chain_strength = 200
 
-sampleset = sampler.sample_qubo(qubo, chain_strength=chain_strength, num_reads=1000)
+sampleset = embedding_sampler.sample_qubo(qubo, chain_strength=chain_strength, num_reads=1000)
 
 
 print(sampleset)
